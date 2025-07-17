aa32c0217f756f66fdf3eae357d2809e591a8cce
Shashank Pathak <shashankpk34@gmail.com>
2025-07-18 00:54:16 +0530

    working for register user apigateway

--- api-gateway/src/main/java/com/railway/api_gateway/controller/AuthController.java
+++ api-gateway/src/main/java/com/railway/api_gateway/controller/AuthController.java
@@ -5,10 +5,8 @@ import com.railway.api_gateway.security.CustomReactiveUserDetailsService;
 import com.railway.api_gateway.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,18 +15,23 @@ import reactor.core.publisher.Mono;
 
 @RestController
 public class AuthController {
-	@Autowired
 	private ReactiveAuthenticationManager authenticationManager;
-	@Autowired
 	private CustomReactiveUserDetailsService userDetailsService;
-	@Autowired
 	private JwtUtil jwtUtil;
 	
+	@Autowired
+	public AuthController(ReactiveAuthenticationManager authenticationManager,
+			CustomReactiveUserDetailsService userDetailsService, JwtUtil jwtUtil) {
+		this.authenticationManager = authenticationManager;
+		this.jwtUtil = jwtUtil;
+		this.userDetailsService = userDetailsService;
+	}
+
 	@GetMapping("/auth/home")
 	public Mono<ResponseEntity<String>> home() {
 		return Mono.just(ResponseEntity.ok("Welcome to the authentication home page!"));
 	}
-	
+
 	@PostMapping("/auth/register")
 	public Mono<ResponseEntity<String>> register(@RequestBody User user) {
 		return userDetailsService.registerUser(user)
@@ -37,11 +40,13 @@ public class AuthController {
 
 	@PostMapping("/auth/login")
 	public Mono<ResponseEntity<String>> login(@RequestBody LoginRequest loginRequest) {
-	    return authenticationManager.authenticate(
-	            new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()))
-	            .flatMap(auth -> userDetailsService.findByUsername(loginRequest.getUsername())
-	                    .map(userDetails -> ResponseEntity.ok(jwtUtil.generateToken(userDetails.getUsername(), userDetails.getAuthorities().iterator().next().getAuthority())))); 
-	    }
+		return authenticationManager
+				.authenticate(
+						new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()))
+				.flatMap(auth -> userDetailsService.findByUsername(loginRequest.getUsername())
+						.map(userDetails -> ResponseEntity.ok(jwtUtil.generateToken(userDetails.getUsername(),
+								userDetails.getAuthorities().iterator().next().getAuthority()))));
+	}
 }
 
 class LoginRequest {
