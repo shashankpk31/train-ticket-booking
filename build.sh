c065f56ea7198aa757482602ec24b09ae805b685
Shashank Pathak <shashankpk34@gmail.com>
2025-07-20 19:45:20 +0530

    issue in docker container

---
+++ build.sh
@@ -0,0 +1,18 @@
+cd ./api-gateway
+mvn clean package
+cd ..
+cd ./train-service
+mvn clean package
+cd ..
+cd ./inventory-service
+mvn clean package
+cd .. 
+cd ./notification-service 
+mvn clean package
+cd ..
+cd ./booking-service
+mvn clean package
+cd ..
+docker compose build api-gateway train-service inventory-service notification-service booking-service
+docker compose up -d
+docker image prune
